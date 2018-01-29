0dfc2359a700a3a49f585aec56a6782278578c9a
kgonzalez452 <kgonzalez@techlaunch.io>
2018-01-29 07:34:43 -0500

    first commit

---
+++ quiz2.js
@@ -0,0 +1,53 @@
+// Instructions: create a new repository called ng-ds-test. Within this repo, create a README.md file. Copy and paste all of these questions into this README file and answer them.
+
+// Name and describe the two main operations of a stack (to add and remove data).
+push(); adds to the beginning of an object.
+pop(); removes from the end of an object.
+// Name and describe the two main operations of a queue (to add and remove data).
+enqueue(); adds a value to the beginning of a queue.
+dequeue(); removes the first value of a queue.
+// Draw the tree resulting from adding the following sequence of numbers to an empty tree: 30, 45, 15, 10, 50, 40, 20, 27
+// 10, 15, 20, 27, 30, 40, 45, 50
+                                            //              30
+                                            //            /    \        
+                                            //          20      45
+                                            //         /  \    /  \
+                                            //       15   27  40  50 
+                                            //      /  
+                                            //    10   
+
+
+
+
+
+// Is this tree balanced? If not, which rotation needs to be done? (Use the following rotation as an example: rightRotation(30), or leftRotation(10))
+    leftRotation(10);
+// Now add 29. Is the tree balanced? If not, which rotation needs to be done? (Use the following rotation as an example: rightRotation(30), or leftRotation(10))
+
+// Consider the following tree:
+// ------5
+// ---2-----8
+// -1--3
+
+// Now add 0 to the tree. Which one is the first node to go out of balance?
+
+// How do you fix this node? (Use the following rotation as an example: rightRotation(30), or leftRotation(10))
+
+// What are the four main steps of mergesort?
+    Divide
+    Conquer
+    Sort
+    Combine
+// Say you have a program which handles the login queue to a game server. The game server is able to log in one person every one second. Assume that one second must elapse after a person logs in with an empty queue before they are removed from the queue. Draw the state of the queue at 12:00:06, considering the following sequence of events:
+
+// At 12:00:00 Hades logs in
+// At 12:00:00 Ares logs in
+// At 12:00:00 Zeus logs in
+// At 12:00:00 Buzz Light Year logs in
+// At 12:00:02 Kanye West logs in
+// At 12:00:03 Taylor Swift logs in
+// At 12:00:03 Darkwing Duck logs in
+// At 12:00:04 Evil Mickey logs in.
+// Solve https://www.hackerrank.com/challenges/compare-the-triplets - be mindful that you are required to print the output to the console in exactly the format that was asked. You are not required to return a value, just print to the console. Also be mindful to use JavaScript.
+
+// Solve https://www.hackerrank.com/challenges/mini-max-sum - be mindful that you are required to print the output to the console in exactly the format that was asked. You are not required to return a value, just print to the console. Also be mindful to use JavaScript.
